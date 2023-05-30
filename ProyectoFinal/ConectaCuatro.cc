b95ffcda0aab9bf4c684f4a74e7948bddbcdf096
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-05-30 13:58:03 +0200

    From/to Bin adaptados para tener tipo, nick y el contenido del mensaje

--- ProyectoFinal/ConectaCuatro.cc
+++ ProyectoFinal/ConectaCuatro.cc
@@ -10,6 +10,19 @@ void ConectaCuatro_Message::to_bin()
     memset(_data, 0, MESSAGE_SIZE);
 
     //Serializar los campos type, nick y message en el buffer _data
+    char *tmp = _data;
+
+    // Un index para identificar el tipo de mensaje 
+    memcpy(tmp, &type, sizeof(uint8_t));
+    tmp +=sizeof(uint8_t);
+
+    // Espacio para el nombre
+    memcpy(tmp, nick.c_str(), sizeof(char) * 8);
+    tmp += 8*sizeof(char);
+
+    // Importante que el tamaño del mensaje pueda almacenar 
+    // el estado del tablero entero, invluyendo los "|" 
+    memcpy(tmp, message.c_str(), sizeof(char) * 300); // 80
 }
 
 int ConectaCuatro_Message::from_bin(char * bobj)
@@ -18,7 +31,17 @@ int ConectaCuatro_Message::from_bin(char * bobj)
 
     memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
 
+
     //Reconstruir la clase usando el buffer _data
+    char *tmp = bobj;
+    
+    memcpy(&type, tmp, sizeof(uint8_t));
+    tmp +=sizeof(uint8_t);
+
+    nick = tmp;
+    tmp += sizeof(char) * 8;
+
+    message = tmp;
 
     return 0;
 }
